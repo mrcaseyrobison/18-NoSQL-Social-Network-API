7e508e394e7732a1a085ed095f00082c48ff38f0
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-10-10 02:01:44 -0400

    routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+
+// Route handler from controller file
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController');
+
+// /api/user
+router.route('/')
+.get(getUsers)
+.post(createUser);
+
+// /api/user/:userId
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser)
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(removeFriend);
+
+module.exports = router;
